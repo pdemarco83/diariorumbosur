978328d9a932f7cbc7e69e17f41f3696f95d0663
Pablo <pablo@ombligoproducciones.com.ar>
2013-11-07 21:21:34 -0300

    tema base

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* The template for displaying the footer.
+*
+* Contains the closing of the id=main div and all content after
+*
+* @package diariorumbosur
+* @since diariorumbosur 1.0
+*/
+?>
+ 
+</div><!-- #main .site-main -->
+ 
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info">
+        <?php do_action( 'diariorumbosur_credits' ); ?>
+        <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'diariorumbosur' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'diariorumbosur' ), 'WordPress' ); ?></a>
+        <span class="sep"> | </span>
+        <?php printf( __( 'Theme: %1$s by %2$s.', 'diariorumbosur' ), 'diariorumbosur', '<a href="http://themediariorumbosurr.com/" rel="designer">Themediariorumbosurr</a>' ); ?>
+    </div><!-- .site-info -->
+</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+ 
+<?php wp_footer(); ?>
+ 
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * diariorumbosur functions and definitions
+ *
+ * @package diariorumbosur
+ * @since diariorumbosur 1.0
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ *
+ * @since diariorumbosur 1.0
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 654; /* pixels */
+
+if ( ! function_exists( 'diariorumbosur_setup' ) ):
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * @since diariorumbosur 1.0
+ */
+function diariorumbosur_setup() {
+ 
+    /**
+     * Custom template tags for this theme.
+     */
+    require( get_template_directory() . '/inc/template-tags.php' );
+ 
+    /**
+     * Custom functions that act independently of the theme templates
+     */
+    require( get_template_directory() . '/inc/tweaks.php' );
+ 
+    /**
+     * Make theme available for translation
+     * Translations can be filed in the /languages/ directory
+     * If you're building a theme based on diariorumbosur, use a find and replace
+     * to change 'diariorumbosur' to the name of your theme in all the template files
+     */
+    load_theme_textdomain( 'diariorumbosur', get_template_directory() . '/languages' );
+ 
+    /**
+     * Add default posts and comments RSS feed links to head
+     */
+    add_theme_support( 'automatic-feed-links' );
+ 
+    /**
+     * Enable support for the Aside Post Format
+     */
+    add_theme_support( 'post-formats', array( 'aside' ) );
+ 
+    /**
+     * This theme uses wp_nav_menu() in one location.
+     */
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'diariorumbosur' ),
+    ) );
+}
+endif; // diariorumbosur_setup
+add_action( 'after_setup_theme', 'diariorumbosur_setup' );
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ *
+ * @since diariorumbosur 1.0
+ */
+function diariorumbosur_widgets_init() {
+    register_sidebar( array(
+        'name' => __( 'Primary Widget Area', 'diariorumbosur' ),
+        'id' => 'sidebar-1',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h1 class="widget-title">',
+        'after_title' => '</h1>',
+    ) );
+ 
+    register_sidebar( array(
+        'name' => __( 'Secondary Widget Area', 'diariorumbosur' ),
+        'id' => 'sidebar-2',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h1 class="widget-title">',
+        'after_title' => '</h1>',
+    ) );
+}
+add_action( 'widgets_init', 'diariorumbosur_widgets_init' );
+
+/**
+ * Enqueue scripts and styles
+ */
+function diariorumbosur_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+ 
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+ 
+    wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+ 
+    if ( is_singular() && wp_attachment_is_image() ) {
+        wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'diariorumbosur_scripts' );
+
+/**
+ * Setup the WordPress core custom background feature.
+ *
+ * Use add_theme_support to register support for WordPress 3.4+
+ * as well as provide backward compatibility for previous versions.
+ * Use feature detection of wp_get_theme() which was introduced
+ * in WordPress 3.4.
+ *
+ * Hooks into the after_setup_theme action.
+ *
+ */
+function diariorumbosur_register_custom_background() {
+    $args = array(
+        'default-color' => 'e9e0d1',
+    );
+ 
+    $args = apply_filters( 'diariorumbosur_custom_background_args', $args );
+ 
+    if ( function_exists( 'wp_get_theme' ) ) {
+        add_theme_support( 'custom-background', $args );
+    } else {
+        define( 'BACKGROUND_COLOR', $args['default-color'] );
+        define( 'BACKGROUND_IMAGE', $args['default-image'] );
+        add_custom_background();
+    }
+}
+add_action( 'after_setup_theme', 'diariorumbosur_register_custom_background' );
+ 
+/**
+
+/**
+ * Implement the Custom Header feature
+ */
+require( get_template_directory() . '/inc/custom-header.php' );
