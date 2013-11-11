02113098d34f29f86bc12d740b3a40f70556e4ae
Pablo <pablo@ombligoproducciones.com.ar>
2013-11-10 23:05:32 -0300

    header-encaminado

--- functions.php
+++ functions.php
@@ -35,25 +35,7 @@ function diariorumbosur_setup() {
      * Custom functions that act independently of the theme templates
      */
     require( get_template_directory() . '/inc/tweaks.php' );
- 
-    /**
-     * Make theme available for translation
-     * Translations can be filed in the /languages/ directory
-     * If you're building a theme based on diariorumbosur, use a find and replace
-     * to change 'diariorumbosur' to the name of your theme in all the template files
-     */
-    load_theme_textdomain( 'diariorumbosur', get_template_directory() . '/languages' );
- 
-    /**
-     * Add default posts and comments RSS feed links to head
-     */
-    add_theme_support( 'automatic-feed-links' );
- 
-    /**
-     * Enable support for the Aside Post Format
-     */
-    add_theme_support( 'post-formats', array( 'aside' ) );
- 
+    
     /**
      * This theme uses wp_nav_menu() in one location.
      */
@@ -96,16 +78,9 @@ add_action( 'widgets_init', 'diariorumbosur_widgets_init' );
 function diariorumbosur_scripts() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
  
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
- 
     wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
  
-    if ( is_singular() && wp_attachment_is_image() ) {
-        wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-    }
-}
+ }
 add_action( 'wp_enqueue_scripts', 'diariorumbosur_scripts' );
 
 /**
@@ -119,26 +94,3 @@ add_action( 'wp_enqueue_scripts', 'diariorumbosur_scripts' );
  * Hooks into the after_setup_theme action.
  *
  */
-function diariorumbosur_register_custom_background() {
-    $args = array(
-        'default-color' => 'e9e0d1',
-    );
- 
-    $args = apply_filters( 'diariorumbosur_custom_background_args', $args );
- 
-    if ( function_exists( 'wp_get_theme' ) ) {
-        add_theme_support( 'custom-background', $args );
-    } else {
-        define( 'BACKGROUND_COLOR', $args['default-color'] );
-        define( 'BACKGROUND_IMAGE', $args['default-image'] );
-        add_custom_background();
-    }
-}
-add_action( 'after_setup_theme', 'diariorumbosur_register_custom_background' );
- 
-/**
-
-/**
- * Implement the Custom Header feature
- */
-require( get_template_directory() . '/inc/custom-header.php' );
--- header.php
+++ header.php
@@ -40,8 +40,6 @@ if ( $paged >= 2 || $page >= 2 )
 echo ' | ' . sprintf( __( 'Page %s', 'diariorumbosur' ), max( $paged, $page ) );
  
 ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
@@ -51,22 +49,24 @@ echo ' | ' . sprintf( __( 'Page %s', 'diariorumbosur' ), max( $paged, $page ) );
 <body <?php body_class(); ?>>
 
 <div id="page" class="hfeed site">
-     <header id="masthead" class="site-header" role="banner">
-        <hgroup>
-		     <h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		     <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
-		<?php $header_image = get_header_image();
-        if ( ! empty( $header_image ) ) { ?>
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-                <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-            </a>
-		<?php } // if ( ! empty( $header_image ) ) 
-		?>
-         <nav role="navigation" class="site-navigation main-navigation">
-		 	<h1 class="assistive-text"><?php _e( 'Menu', 'diariorumbosur' ); ?></h1>
-		 	<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', 'diariorumbosur' ); ?></a></div>
-		 	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-	 	</nav><!-- .site-navigation .main-navigation -->
-     </header><!-- #masthead .site-header -->
-<div id="main" class="site-main">
+	<div class="pre-header">
+		<div class="fecha">Lunes 23 de septiembre de 2013</div>
+		<div class="tiempo">Buenos Aires, Argentina</div>			
+		<div class="social">F T M</div>	
+	</div>
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header">
+			<div class="logo">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+            		<img src="<?php echo get_bloginfo('template_directory');?>/img/diario-rumbo-sur-logo.jpg">
+            	</a>
+			</div>
+			<nav role="navigation" class="site-navigation main-navigation">
+				<h1 class="assistive-text"><?php _e( 'Menu', 'diariorumbosur' ); ?></h1>
+				<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', 'diariorumbosur' ); ?></a></div>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+ 			</nav><!-- .site-navigation .main-navigation -->
+		</div>				
+	</header>
+	<div class="ultimo-momento">LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO &nbsp LLUVIA TORRENCIAL EN CABALLITO </div>
+	<div id="main" class="site-main">
